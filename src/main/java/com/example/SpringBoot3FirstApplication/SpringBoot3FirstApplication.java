24224e81bd220975d012081cfcc6ce2cf028b790
Ajaty <jesseossvargas@gmail.com>
2023-09-07 12:47:53 -0300

    Inicializar Repositorio

---
+++ src/main/java/com/example/SpringBoot3FirstApplication/SpringBoot3FirstApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot3FirstApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3FirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3FirstApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SpringBoot3FirstApplication/SpringBoot3FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot3FirstApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
